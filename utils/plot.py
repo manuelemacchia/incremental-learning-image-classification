66f329c65a4756f79b041133fb1c078ef320826f
Manuele Macchia <macchiamanuele@gmail.com>
2020-05-21 12:24:33 +0200

    move plot_test_scores to utils module (renamed to test_scores)

--- utils/plot.py
+++ utils/plot.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import torch
 import torchvision
 
+
 def image_grid(images, one_channel=False):
     """Show an image grid.
 
@@ -19,12 +20,13 @@ def image_grid(images, one_channel=False):
 
     if one_channel:
         img_grid = img_grid.mean(dim=0)
-    img_grid = img_grid / 2 + 0.5 # Unnormalize
+    img_grid = img_grid / 2 + 0.5  # Unnormalize
     npimg = img_grid.numpy()
     if one_channel:
         plt.imshow(npimg, cmap="Greys")
     else:
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
 
 def train_val_scores(train_loss, train_accuracy, validation_loss, validation_accuracy, save_directory):
     """Plot loss and accuracy for training and validation
@@ -50,7 +52,7 @@ def train_val_scores(train_loss, train_accuracy, validation_loss, validation_acc
     axes[0].errorbar(x, np.array(validation_loss)[:, 0], np.array(validation_loss)[:, 1],
                      color='#FF9232', linewidth=2.5, label='Validation')
     axes[0].set_title("Training and validation loss")
-    axes[0].set_xlabel("Number of classes trained")
+    axes[0].set_xlabel("Number of classes")
     axes[0].set_ylabel("Loss")
 
     axes[1].errorbar(x, np.array(train_accuracy)[:, 0], np.array(train_accuracy)[:, 1],
@@ -58,7 +60,7 @@ def train_val_scores(train_loss, train_accuracy, validation_loss, validation_acc
     axes[1].errorbar(x, np.array(validation_accuracy)[:, 0], np.array(validation_accuracy)[:, 1],
                      color='#FF9232', linewidth=2.5, label='Validation')
     axes[1].set_title("Training and validation accuracy")
-    axes[1].set_xlabel("Number of classes trained")
+    axes[1].set_xlabel("Number of classes")
     axes[1].set_ylabel("Accuracy")
 
     # Layout
@@ -70,7 +72,40 @@ def train_val_scores(train_loss, train_accuracy, validation_loss, validation_acc
     axes[1].legend()
     axes[1].grid(True)
 
-    # Save if directory is specified
+    # Save image if directory is specified
+    if save_directory != None:
+        fig.savefig(save_directory)
+
+    plt.show()
+
+
+def test_scores(test_accuracy, save_directory=None):
+    """Plot accuracy for test
+    
+    Args:
+        test_accuracy: mean and standard deviation of test accuracy
+        save_directory (path): if specified, save the plot as a file in this
+            directory
+    """
+
+    fig, ax = plt.subplots(1, 1, figsize=[15, 5])
+
+    x = np.arange(10, 101, 10)
+
+    # Use errorbar to plot the standard deviation between different runs
+    ax.errorbar(x, np.array(test_accuracy)[:, 0], np.array(test_accuracy)[:, 1],
+                  color='#2E84D5', linewidth=2.5)
+    ax.set_title("Test accuracy")
+    ax.set_xlabel("Number of classes")
+    ax.set_ylabel("Accuracy")
+
+    # Layout
+    plt.tight_layout()
+
+    ax.legend()
+    ax.grid(True)
+
+    # Save image if directory is specified
     if save_directory != None:
         fig.savefig(save_directory)
 
