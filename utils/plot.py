9c40b2737da53f5dcd9c3e07701c643bb078a94e
Manuele Macchia <macchiamanuele@gmail.com>
2020-05-21 12:15:00 +0200

    move plot_train_scores to utils package (renamed to train_val_scores)

--- utils/plot.py
+++ utils/plot.py
@@ -8,7 +8,7 @@ import torchvision
 
 def image_grid(images, one_channel=False):
     """Show an image grid.
-    
+
     Args:
         images (torch.Tensor): batch of images to show
         one_channel (bool): if True, show images in grayscale, otherwise
@@ -19,9 +19,59 @@ def image_grid(images, one_channel=False):
 
     if one_channel:
         img_grid = img_grid.mean(dim=0)
-    img_grid = img_grid / 2 + 0.5 # unnormalize
+    img_grid = img_grid / 2 + 0.5 # Unnormalize
     npimg = img_grid.numpy()
     if one_channel:
         plt.imshow(npimg, cmap="Greys")
     else:
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+def train_val_scores(train_loss, train_accuracy, validation_loss, validation_accuracy, save_directory):
+    """Plot loss and accuracy for training and validation
+
+    The plot contains two axes:
+        axes[0] is the loss plot
+        axes[1] is the accuracy plot
+
+    Args:
+        train_loss, train_accuracy, validation_loss, validation_accuracy:
+            mean and standard deviation with comparable x axis
+        save_directory (path): if specified, save the plot as a file in this
+            directory
+    """
+
+    fig, axes = plt.subplots(1, 2, figsize=[15, 5])
+
+    x = np.arange(10, 101, 10)
+
+    # Use errorbar to plot the standard deviation between different runs
+    axes[0].errorbar(x, np.array(train_loss)[:, 0], np.array(train_loss)[:, 1],
+                     color='#2E84D5', linewidth=2.5, label='Training')
+    axes[0].errorbar(x, np.array(validation_loss)[:, 0], np.array(validation_loss)[:, 1],
+                     color='#FF9232', linewidth=2.5, label='Validation')
+    axes[0].set_title("Training and validation loss")
+    axes[0].set_xlabel("Number of classes trained")
+    axes[0].set_ylabel("Loss")
+
+    axes[1].errorbar(x, np.array(train_accuracy)[:, 0], np.array(train_accuracy)[:, 1],
+                     color='#2E84D5', linewidth=2.5, label='Training')
+    axes[1].errorbar(x, np.array(validation_accuracy)[:, 0], np.array(validation_accuracy)[:, 1],
+                     color='#FF9232', linewidth=2.5, label='Validation')
+    axes[1].set_title("Training and validation accuracy")
+    axes[1].set_xlabel("Number of classes trained")
+    axes[1].set_ylabel("Accuracy")
+
+    # Layout
+    plt.tight_layout()
+
+    axes[0].legend()
+    axes[0].grid(True)
+
+    axes[1].legend()
+    axes[1].grid(True)
+
+    # Save if directory is specified
+    if save_directory != None:
+        fig.savefig(save_directory)
+
+    plt.show()
