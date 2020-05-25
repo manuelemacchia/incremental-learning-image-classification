0fff7880297d9c9ce835a2c112770dce619883ef
Manuele Macchia <macchiamanuele@gmail.com>
2020-05-25 23:48:12 +0200

    change the number of output nodes of resnet to 100

--- model/resnet_cifar.py
+++ model/resnet_cifar.py
@@ -86,7 +86,7 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, num_classes=10):
+    def __init__(self, block, layers, num_classes=100):
         self.inplanes = 16
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1,
