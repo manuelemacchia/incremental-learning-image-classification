d64fbda3a9cf1bd093398f33d4b70164ed9a9a94
Manuele Macchia <macchiamanuele@gmail.com>
2020-06-01 12:44:22 +0200

    create LWF class in model

---
+++ model/lwf.py
@@ -0,0 +1,14 @@
+from manager import Manager
+
+import torch
+import torch.nn as nn
+from torch.backends import cudnn
+
+class LWF():
+    """Learning without Forgetting
+    
+    @todo: docs
+    """
+
+    def __init__(self):
+        pass
